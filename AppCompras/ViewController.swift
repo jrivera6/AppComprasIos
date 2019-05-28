581eafab3c155566dd455ba3c57a7534f05d319a
Jhonny Rivera <jhonny.riverap6@gmail.com>
2019-05-28 14:08:25 -0500

    Initial Commit

---
+++ AppCompras/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AppCompras
+//
+//  Created by Jhonny Rivera on 5/28/19.
+//  Copyright © 2019 Tecsup. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
